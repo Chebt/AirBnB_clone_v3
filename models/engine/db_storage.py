fc157ff441c8b4f511002f54d08d23f1b8510337
chebt <chebetdaniel590@gmail.com>
2023-02-17 17:25:42 +0000

    solo done

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,6 @@
 """
 Contains the class DBStorage
 """
-
 import models
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
@@ -74,3 +73,21 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Query object based on id & class"""
+        obj = self.__session.query(cls).filter_by(id=id).first()
+        if obj:
+            return obj
+        else:
+            return None
+
+    def count(self, cls=None):
+        """Count objects based (or not) on class"""
+        if cls:
+            count = self.__session.query(cls).count()
+        else:
+            count = 0
+            for model in classes.values():
+                count += self.__session.query(model).count()
+        return count
